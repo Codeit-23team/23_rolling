69db42d0b3e34626a09d73d9c9045adb8de10242
skoo1100 <skoo062125@gmail.com>
2024-02-26 16:14:30 +0900

    add: button, textfield components

---
+++ src/components/button/buttonPrimary/buttonPrimary40/buttonPrimary40.jsx
@@ -0,0 +1,7 @@
+import './buttonPrimary40.css';
+
+const ButtonPrimary40 = ({ buttonName }) => {
+  return <button className="buttonPrimary40">{buttonName}</button>;
+};
+
+export default ButtonPrimary40;
---
+++ src/components/textField/dropdown/textDropdown.jsx
@@ -0,0 +1,12 @@
+import './textDropdown.css';
+
+const TextDropdown = () => {
+  return (
+    <select className="textDropdown">
+      <option>1</option>
+      <option>2</option>
+    </select>
+  );
+};
+
+export default TextDropdown;
