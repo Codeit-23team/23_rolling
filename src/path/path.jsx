c8174dedadc92dc9926d7bf8957d29c49a337c93
skoo1100 <skoo062125@gmail.com>
2024-02-27 15:40:25 +0900

    feat: /post/{id} 페이지 이동

--- src/path/path.jsx
+++ src/path/path.jsx
@@ -1,16 +1,23 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Index from '../pages';
 import PostPage from '../pages/post';
+import PostIdPage from '../pages/post/id';
 import PostMessagePage from '../pages/post/id/message';
 import ListPage from '../pages/list';
+import { useRecoilValue } from 'recoil';
+import { userId } from '../store/recoil/apiData';
 
 function Path() {
+  const selectId = useRecoilValue(userId);
+
+  console.log(`post/${selectId}`);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="message" element={<PostMessagePage />} />
         <Route path="post" element={<PostPage />} />
+        <Route path={'post/' + selectId} element={<PostIdPage selectId={selectId} />} />
         <Route path="list" element={<ListPage />} />
       </Routes>
     </BrowserRouter>
