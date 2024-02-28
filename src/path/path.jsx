edd3018cdc1750643302ea7792da7b26d27d60a6
skoo1100 <skoo062125@gmail.com>
2024-02-28 16:19:16 +0900

    fix: 나은님 conflict 충돌 해결

--- src/path/path.jsx
+++ src/path/path.jsx
@@ -1,4 +1,3 @@
-
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Index from '../pages';
 import PostPage from '../pages/post';
@@ -7,13 +6,6 @@ import PostMessagePage from '../pages/post/id/message';
 import ListPage from '../pages/list';
 
 function Path() {
-<<<<<<< HEAD
-=======
-  const selectId = useRecoilValue(userId);
-
-  console.log(`post/${selectId}`);
-
->>>>>>> 9d07facf89c3bcfbb775d67f5b557edbd41fa7e0
   return (
     <BrowserRouter>
       <Routes>
@@ -21,10 +13,7 @@ function Path() {
 
         <Route path="message" element={<PostMessagePage />} />
         <Route path="post" element={<PostPage />} />
-<<<<<<< HEAD
-=======
         <Route path="/post/:id" element={<PostIdPage />} />
->>>>>>> 9d07facf89c3bcfbb775d67f5b557edbd41fa7e0
         <Route path="list" element={<ListPage />} />
       </Routes>
     </BrowserRouter>
