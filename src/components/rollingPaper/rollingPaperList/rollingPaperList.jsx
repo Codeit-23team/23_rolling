fc234a373b2b23ccc7a8b9fbf5e0242edd19420d
Lim dong hyeon <ilhai3@naver.com>
2024-02-29 13:06:47 +0900

    feat: swiper 기능 동작

--- src/components/rollingPaper/rollingPaperList/rollingPaperList.jsx
+++ src/components/rollingPaper/rollingPaperList/rollingPaperList.jsx
@@ -2,6 +2,15 @@ import { useEffect, useState } from 'react';
 import { getApiRecipientList } from '../../../apis/apiRecipient';
 import RollingPaperBox from '../rollingPaperBox/rollingPaperBox';
 import './rollingPaperList.css';
+import { Link } from 'react-router-dom';
+import { Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 function RollingPaperList({ name }) {
   const [userData, setUserData] = useState(undefined);
@@ -16,20 +25,40 @@ function RollingPaperList({ name }) {
   return (
     <div className="rollingPaperList">
       <h1>{name}</h1>
-      <ul className="rollingPaperBoxList">
+      {/* <ul className="rollingPaperBoxList">
         {userData?.map((data) => (
           <li key={data.id}>
-            <RollingPaperBox
-              name={data.name}
-              backgroundColor={data.backgroundColor}
-              reactionCount={data.reactionCount}
-              topReactions={data.topReactions}
-            />
+            <Link to={`/post/${data.id}`}>
+              <RollingPaperBox
+                name={data.name}
+                backgroundColor={data.backgroundColor}
+                reactionCount={data.reactionCount}
+                topReactions={data.topReactions}
+              />
+            </Link>
           </li>
         ))}
-      </ul>
-      <button className="left">{`<`}</button>
-      <button className="right">{`>`}</button>
+      </ul> */}
+      <Swiper
+        className='rollingPaperBoxList'
+        modules={[Navigation]}
+        spaceBetween={20}
+        slidesPerView={4}
+        navigation
+      >
+        {userData?.map((data) => (
+          <SwiperSlide key={data.id}>
+            <Link to={`/post/${data.id}`}>
+              <RollingPaperBox
+                name={data.name}
+                backgroundColor={data.backgroundColor}
+                reactionCount={data.reactionCount}
+                topReactions={data.topReactions}
+              />
+            </Link>
+          </SwiperSlide>
+        ))}
+    </Swiper>
     </div>
   );
 }
