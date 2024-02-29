774e0f98acad84122f051995f02ab8caf7855a2c
Lim dong hyeon <ilhai3@naver.com>
2024-02-29 11:50:14 +0900

    간격 수정

--- src/pages/post/id/message/index.jsx
+++ src/pages/post/id/message/index.jsx
@@ -6,26 +6,19 @@ import ProfileImage from '../../../../components/profileImage/profileImage';
 import TextDropdown from '../../../../components/textField/dropdown/textDropdown';
 import './index.css';
 import { useRecoilState } from 'recoil';
-import {
-  messageRelationship,
-  messageFont,
-  messageEditor,
-  fromName,
-  messageProfile,
-} from '../../../../store/recoil/apiData';
-import { useEffect } from 'react';
+import { messageRelationship, messageFont, messageEditor, fromName, messageProfile} from '../../../../store/recoil/apiData';
 import { messageApiRecipient } from '../../../../apis/apiRecipient';
 import { useParams, useNavigate } from 'react-router-dom';
 
 const PostMessagePage = () => {
-  const [messageName, setMessageName] = useRecoilState(fromName);
-  const [messageImage, setMessageImage] = useRecoilState(messageProfile);
-  const [relationship, setRelationship] = useRecoilState(messageRelationship);
-  const [editorData, setEditorData] = useRecoilState(messageEditor);
-  const [choiceFont, setChoiceFont] = useRecoilState(messageFont);
-  const { id } = useParams();
-  const navigate = useNavigate();
-  const relationshipOptions = [
+    const [messageName, setMessageName] = useRecoilState(fromName);
+    const [messageImage, setMessageImage] = useRecoilState(messageProfile);
+    const [relationship, setRelationship] = useRecoilState(messageRelationship);
+    const [editorData, setEditorData] = useRecoilState(messageEditor);
+    const [choiceFont, setChoiceFont] = useRecoilState(messageFont);
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const relationshipOptions = [
     { value: '친구', label: '친구' },
     { value: '지인', label: '지인' },
     { value: '동료', label: '동료' },
@@ -48,31 +41,38 @@ const PostMessagePage = () => {
       );
     }
   };
-  return (
-    <>
-      <Header button={false}></Header>
-      <div className="messageContainer">
-        <InputName
-          setUserName={setMessageName}
-          type={'From'}
-          holdedrType={'이름을 입력해 주세요.'}
-        />
-        <ProfileImage profileImage={messageImage} setProfileImage={setMessageImage} />
-        <TextDropdown
-          setValue={setRelationship}
-          options={relationshipOptions}
-          type={'상대와의 관계'}
-        />
-        <Editor
-          editorData={editorData}
-          setEditorData={setEditorData}
-          type={'내용을 입력해주세요'}
-        />
-        <TextDropdown setValue={setChoiceFont} options={fontOptions} type={'폰트 선택'} />
-        <ButtonPrimary56 handleApi={handlePostMessage} buttonName={'생성하기'} />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Header button={false}></Header>
+            <div className="messageContainer">
+                <InputName
+                setUserName={setMessageName}
+                type={'From'}
+                holdedrType={'이름을 입력해 주세요.'}
+                />
+                <ProfileImage 
+                profileImage={messageImage} 
+                setProfileImage={setMessageImage} />
+                <TextDropdown
+                setValue={setRelationship}
+                options={relationshipOptions}
+                type={'상대와의 관계'}
+                />
+                <Editor
+                editorData={editorData}
+                setEditorData={setEditorData}
+                type={'내용을 입력해주세요'}
+                />
+                <TextDropdown 
+                setValue={setChoiceFont}
+                options={fontOptions} 
+                type={'폰트 선택'} />
+                <ButtonPrimary56 
+                handleApi={handlePostMessage} 
+                buttonName={'생성하기'} />
+            </div>
+        </>
+    );
 };
 
 export default PostMessagePage;
